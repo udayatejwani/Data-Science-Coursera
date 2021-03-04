311997cbe5b6469915d3b61f0c1320b2c94bd9cc
udayatejwani <udayatejwani@hotmail.com>
2021-03-04 13:54:17 -0700

    Add files via upload

--- Developing Data Products/Week 4 Project/server.R
+++ Developing Data Products/Week 4 Project/server.R
@@ -14,27 +14,31 @@ data(mtcars)
 
 # Define server logic required to draw a regression model representation
 shinyServer(function(input, output) {
-   
-  # Fit regression line mpg Vs. wt
   
-  require(stats)
-  reg <- lm(mpg ~ wt, data = mtcars)
-  coeff = coefficients(reg)
-  
-  ltext <- paste("mpg = ", round(coeff[2],1), " * wt + ", round(coeff[1],1))
-  
-  output$plot1 <- renderPlot({
+   output$plot1 <- renderPlot({
+     
     minX <- input$sliderX[1] 
     maxX <- input$sliderX[2] 
     minY <- input$sliderY[1] 
-    maxY <- input$sliderY[2] 
+    maxY <- input$sliderY[2]
+
+    # Fit regression line mpg Vs. wt
+    testdata <- subset(mtcars, subset=(mpg > minY & mpg < maxY & wt > minX & wt < maxX ))
+    require(stats)
+    reg <- lm(mpg ~ wt, data = testdata)
+    coeff = coefficients(reg)
     
-    gp <- ggplot(mtcars, aes(wt, mpg, color=cyl)) +
-      geom_point() +
+    ltext <- paste("mpg = ", round(coeff[2],1), " * wt + ", round(coeff[1],1))
+    
+    cols <- c("8" = "red", "7" = "pink", "6" = "blue", "5" = "green", "4" = "yellow")
+    
+    gp <- ggplot(mtcars, aes(wt, mpg, colour = factor(cyl), fill = factor(cyl))) +
+      geom_point(shape = 21, alpha = 0.5, size = 2) + scale_color_manual(values = cols, breaks = c("4", "5", "6", "7", "8"), aesthetics = c("colour","fill")) +
       xlim(minX, maxX) +
-      ylim(minY, maxY)
-    
-    gp + geom_abline(intercept = coeff[1], slope = coeff[2], col="green", size = 1.5) + geom_text(x = minX + 2, y = (minY + maxY)/2, label = ltext, size = 10)
+      ylim(minY, maxY) 
+     
+    gp + geom_abline(intercept = coeff[1], slope = coeff[2], col="green", size = 1.5) + 
+    geom_text(x = minX + 0.8, y = minY, label = ltext, size = 5, color = "black")
   })
   
 })
--- Developing Data Products/Week 4 Project/ui.R
+++ Developing Data Products/Week 4 Project/ui.R
@@ -12,13 +12,31 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("The Green Car"),
+  navbarPage("MPG Vs Wt Shiny Application",
+    tabPanel("Instructions",
+      helpText("You work for Motor Trend, a car manufacturing company looking at a data set of a collection of cars. ",
+               "The company is interested in exploring the relationship between weight of car and miles per gallon (MPG) ",
+               "(outcome). They are particularly interested in knowing the optimum weight of car that would provide ",
+               "maximum mpg for a given number of cylinders of engine.  For this purpose they would like to study the ",
+               "regression model for each possible scenario."),  
+      hr(),
+      helpText("The application is available by clicking on the 'Analysis' tab. The use of the application is self ",
+               "explanatory.  Please adjust appropriate slider to appropriate values to focus on specific are of the ",
+               "scatterplot.  The display panel will automatically update and will also display the regression model ",
+               "that is specific to the data that is visible in the display panel."),
+      hr(),
+      helpText("Please note that the application will give you an error 'Error: 0 (non-NA) cases' if you move the slider to values where no data ",
+               "points are available.")
+      ),
+
+    tabPanel("Analysis",
+      titlePanel("The Green Car"),
   
   # Sidebar with a slider input for minimum and maximum values for x-axis and y-axis 
   sidebarLayout(
     sidebarPanel(
-      sliderInput("sliderX", "Pick Minimum and Maximum X Values", 0, 6, value = c(0, 6)),
-      sliderInput("sliderY", "Pick Minimum and Maximum Y Values", 0, 25, value = c(0, 25))
+      sliderInput("sliderX", "Pick Minimum and Maximum Weight Values", 0, 6, value = c(0, 6), step = 0.2),
+      sliderInput("sliderY", "Pick Minimum and Maximum MPG Values", 0, 25, value = c(0, 25))
     ),
     # Show a plot 
     mainPanel(
@@ -26,4 +44,7 @@ shinyUI(fluidPage(
        plotOutput("plot1")
     )
   )
-))
+)
+)
+)
+)
